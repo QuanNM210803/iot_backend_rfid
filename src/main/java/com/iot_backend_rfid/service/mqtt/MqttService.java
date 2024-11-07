5b93223bc3468ea9e05058074b74840eb4175d42
quan <nnmhqn2003@gmail.com>
2024-11-07 20:47:31 +0700

    base code

--- src/main/java/com/iot_backend_rfid/service/mqtt/MqttService.java
+++ src/main/java/com/iot_backend_rfid/service/mqtt/MqttService.java
@@ -1,8 +1,12 @@
-package com.iot_backend_rfid.mqtt;
+package com.iot_backend_rfid.service.mqtt;
 
 import com.iot_backend_rfid.common.Constants;
+import lombok.RequiredArgsConstructor;
 import org.eclipse.paho.client.mqttv3.*;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Service
 public class MqttService implements MqttCallback {
@@ -29,12 +33,20 @@ public class MqttService implements MqttCallback {
 
     }
 
-
-
     @Override
     public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
         String payload = new String(mqttMessage.getPayload());
+
         System.out.println("Received message: " + payload + " from topic: " + topic);
+
+        String[] data=payload.split("&&");
+
+        int roomid=Integer.parseInt(data[0]);
+        String cardid=data[1];
+
+        DateTimeFormatter formatter=DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime dateTime=LocalDateTime.parse(data[2], formatter);
+
     }
 
     @Override
